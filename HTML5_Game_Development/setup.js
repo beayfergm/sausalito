aa11cb607f9fa89d38cc542a8e8909a63d86704a
Fernando Matarrubia <beayfergm@gmail.com>
2015-08-16 17:02:18 -0700

    First commit. Adding some initial HTML test code

---
+++ HTML5_Game_Development/setup.js
@@ -0,0 +1,23 @@
+setup = function() {
+	var body = document.getElementById('body');
+	var canvas = document.createElement('canvas');
+
+	var ctx = canvas.getContext('2d');
+	
+	canvas.width = window.innerWidth;
+	canvas.height = window.innerHeight;
+
+	body.appendChild(canvas);
+
+    img = new Image();
+    img.onload = onImageLoad;
+    img.src = "media/ralphyrobot.png";
+    
+};
+
+onImageLoad = function(){
+    console.log("Image Downloaded!!");
+};
+
+setup();
+
